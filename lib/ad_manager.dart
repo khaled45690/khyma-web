9b7b4996127281f136a09792eba5e929855c400f
alyHwaya <alyappleid4business@gmail.com>
2021-04-13 15:29:28 +0200

    HomeFood Quraan ok prayer ok show date picker For release pwa 4

---
+++ lib/ad_manager.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+class AdManager {
+
+  static String get appId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-9037650239384734~6453608340";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-9037650239384734~8667518777";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-9037650239384734/4182668227";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-9037650239384734/8475947085";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  // static String get interstitialAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>";
+  //   } else if (Platform.isIOS) {
+  //     return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
+  //
+  // static String get rewardedAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "<YOUR_ANDROID_REWARDED_AD_UNIT_ID>";
+  //   } else if (Platform.isIOS) {
+  //     return "<YOUR_IOS_REWARDED_AD_UNIT_ID>";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
+
+
+}
