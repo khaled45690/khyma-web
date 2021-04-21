1605e0d13dfcd2c8bae8153437b575a4b07e456a
alyHwaya <alyappleid4business@gmail.com>
2021-04-21 12:00:55 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-
+// main
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
